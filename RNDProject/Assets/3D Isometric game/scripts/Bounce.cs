8ea582f83b001f7e1db501970b062d85f55f7a2b
TriDivide <liamsmitha@gmail.com>
2018-07-07 11:10:07 +0100

    - fixed movement logic - added animations for movement

--- RNDProject/Assets/3D Isometric game/scripts/Bounce.cs
+++ RNDProject/Assets/3D Isometric game/scripts/Bounce.cs
@@ -11,16 +11,24 @@ public class Bounce : MonoBehaviour {
     Vector3 startPosition, endPosition;
 
 
+    bool firstInput;
+
+    public bool justJump;
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("up") || Input.GetButtonDown("down") || Input.GetButtonDown("left") || Input.GetButtonDown("right")) {
             if (perc == 1) {
                 lerpTime = 1;
                 currentLerpTime = 0;
+                firstInput = true;
+                justJump = true;
             }
         }
+        // get the position of the player before movement
         startPosition = gameObject.transform.position;
         
+        // setting the position of the player after movement
         if (Input.GetButtonDown("right") && gameObject.transform.position == endPosition) {
             endPosition = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
         }
@@ -34,8 +42,17 @@ public class Bounce : MonoBehaviour {
             endPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
         }
 
-        currentLerpTime += Time.deltaTime + 5.5F;
-        perc = currentLerpTime / lerpTime;
-        gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, perc);
+        // Use a lerp to move the player to the end position calculated above
+        if (firstInput) {
+            currentLerpTime += Time.deltaTime;
+            perc = currentLerpTime / lerpTime;
+            gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, perc);
+            if (perc > 0.8) {
+                perc = 1;
+            }
+            if (Mathf.Round(perc) == 1) {
+                justJump = false;
+            }
+        }
     }
 }
