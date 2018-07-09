ac4336b40c132c66590b4b2ba262a0d47a7f9f61
TriDivide <liamsmitha@gmail.com>
2018-07-09 20:26:40 +0100

    - started working on a coroutine to get the water spawning working

--- RNDProject/Assets/3D Isometric game/scripts/WaterMovement.cs
+++ RNDProject/Assets/3D Isometric game/scripts/WaterMovement.cs
@@ -7,7 +7,7 @@ public class WaterMovement : MonoBehaviour {
     float lerpMovement;
     Vector3 startPosition, endPosition;
     float spawnTime;
-    float waterHeightPercent;
+    int waterHeightPercent;
     float perc;
     float lerpTime;
     float currentLerpTime;
@@ -17,41 +17,47 @@ public class WaterMovement : MonoBehaviour {
 
     // Start is called on init
     private void Start() {
-        water = GameObject.Find("DummyWater");        
+        water = GameObject.Find("DummyWater");
+        StartCoroutine("updateWater");
+
     }
 
 
     // Update is called once per frame
     void Update () {
-        if (perc == 1) {
-            lerpTime = 1;
-            currentLerpTime = 0;
-        }
+    }
 
-        // get the position of the player before movement
-        startPosition = gameObject.transform.position;
-
-        // setting the position of the player after movement
-        endPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
-        
-
-        // Use a lerp to move the player to the end position calculated above
-        currentLerpTime += Time.deltaTime * 5;
-        perc = currentLerpTime / lerpTime;
-        if (waterHeightPercent == 1) {
-            waterHeightPercent = 0.2F;
-            Instantiate(water, gameObject.transform.position, gameObject.transform.rotation);
-            gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, perc);
-            if (perc > 0.8) {
-                perc = 1;
+    IEnumerator updateWater() {
+        for (; ; ) {
+            if (perc == 1) {
+                lerpTime = 1;
+                currentLerpTime = 0;
             }
-        }
-        else {
-            gameObject.transform.localScale += new Vector3(0, waterHeightPercent, 0);
-            waterHeightPercent += waterHeightPercent + 0.2F;
-            if (waterHeightPercent <= 0.9F) {
-                waterHeightPercent = 1;
+
+            // get the position of the player before movement
+            startPosition = gameObject.transform.position;
+
+            // setting the position of the player after movement
+            endPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+            // Use a lerp to move the player to the end position calculated above
+            currentLerpTime += Time.deltaTime * 5;
+            perc = currentLerpTime / lerpTime;
+            if (waterHeightPercent == 100) {
+                waterHeightPercent = 20;
+                Instantiate(water, gameObject.transform.position, gameObject.transform.rotation);
+                gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, perc);
+                if (perc > 0.8) {
+                    perc = 1;
+                }
+            } else {
+                gameObject.transform.localScale += new Vector3(0, waterHeightPercent, 0);
+                waterHeightPercent += waterHeightPercent + 20;
+                if (waterHeightPercent <= 0.9F) {
+                    waterHeightPercent = 1;
+                }
             }
+
+            yield return new WaitForSeconds(5);
         }
     }
 }
