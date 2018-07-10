0419ff39d919ad264f8b2bd7065de4a1af049ad6
TriDivide <liamsmitha@gmail.com>
2018-07-10 21:55:21 +0100

    - continued with trying to work on getting the water moving

--- RNDProject/Assets/3D Isometric game/scripts/WaterMovement.cs
+++ RNDProject/Assets/3D Isometric game/scripts/WaterMovement.cs
@@ -7,6 +7,9 @@ public class WaterMovement : MonoBehaviour {
     float waterHeightPercent;
     float height = 0.2F;
 
+    float time = 0;
+
+
     private GameObject water;
 
 
@@ -14,38 +17,37 @@ public class WaterMovement : MonoBehaviour {
     private void Start() {
         water = GameObject.Find("DummyWater");
         gameObject.transform.localScale = new Vector3(0, 0.2F, 0);
-        StartCoroutine("updateWater");
 
     }
 
 
     // Update is called once per frame
     void Update () {
-    }
-
-    IEnumerator updateWater() {
-        for (; ; ) {
-
-            print("current height percent: " + waterHeightPercent);
+      //  print("Time: "+time);
+        //print("waterHeightPercent: " + waterHeightPercent);
+        //print("height: " + height);
+        if (time <= 0) {
+            time = 1.5f;
             if (waterHeightPercent == 1) {
-                print("move the water");
+               // print("move the water");
                 waterHeightPercent = height;
 
                 Instantiate(water, gameObject.transform.position, gameObject.transform.rotation);
-                
-                gameObject.transform.localScale = new Vector3(0, height, 0);
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z -4);
 
+                gameObject.transform.localScale = new Vector3(0, height, 0);
+                print("old local position: "+gameObject.transform.localPosition);
+                Vector3 moveToPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 4);
+                gameObject.transform.Translate(moveToPosition);
+                print("new local position: " +gameObject.transform.localPosition);
             } else {
-                print("set the height of the water");
-                gameObject.transform.localScale += new Vector3(0,  height, 0);
+                //print("set the height of the water");
+                gameObject.transform.localScale = new Vector3(0, height, 0);
                 waterHeightPercent += height;
                 if (waterHeightPercent >= 0.9F) {
                     waterHeightPercent = 1;
                 }
             }
-
-            yield return new WaitForSeconds(1);
-        }
+        }   
+        time -= Time.deltaTime;
     }
 }
