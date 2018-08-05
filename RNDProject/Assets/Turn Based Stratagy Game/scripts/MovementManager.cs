1a7cad0877aab5f5ce06a5b94e7e0008809a540a
TriDivide <liamsmitha@gmail.com>
2018-08-05 18:08:59 +0100

    added code to move units

--- RNDProject/Assets/Turn Based Stratagy Game/scripts/MovementManager.cs
+++ RNDProject/Assets/Turn Based Stratagy Game/scripts/MovementManager.cs
@@ -6,6 +6,9 @@ public class MovementManager : MonoBehaviour {
 
     private GameObject SelectedGridCell;
 
+    //private List<GameObject> SelectedUnits;
+    private GameObject SelectedUnit;
+
 
     public static MovementManager Instance;
 
@@ -23,7 +26,26 @@ public class MovementManager : MonoBehaviour {
 	void Start () {
         GetInstance();
 	}
-	
+
+    public void SelectUnit(GameObject unit) {
+        if (SelectedUnit != null) {
+            SelectedUnit.gameObject.GetComponent<MeshRenderer>().material.color = Color.grey;
+        }
+
+        SelectedUnit = unit;
+
+        SelectedUnit.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+
+    }
+
+    public bool GetIfSelected(GameObject target) {
+        return (target == SelectedUnit);
+    }
+
+
+    public GameObject GetGridCell() {
+        return SelectedGridCell ?? null;
+    }
 
     public void SelectGridCell(GameObject gridCell) {
         if (SelectedGridCell != null) {
---
+++ RNDProject/Assets/Turn Based Stratagy Game/scripts/Unit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    private GameObject SelectedCell;
+
+
+    public float MovementSpeed;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float step = MovementSpeed * Time.deltaTime;
+        SetSelectedCell();
+
+        Debug.Log(MovementManager.Instance.GetIfSelected(gameObject));
+
+        if (SelectedCell != null && MovementManager.Instance.GetIfSelected(gameObject)) {
+            var cellPosition = SelectedCell.transform.position;
+            cellPosition = new Vector3(cellPosition.x, 1.25f, cellPosition.z);
+            transform.position = Vector3.MoveTowards(transform.position, cellPosition, step);
+            if (cellPosition == gameObject.transform.position) {
+                MovementManager.Instance.SelectGridCell(null);
+            }
+        }
+
+    }
+
+    void OnMouseOver() {
+        Debug.Log("Select a unit");
+        if (Input.GetMouseButtonDown(1)) {
+            MovementManager.Instance.SelectUnit(gameObject);
+        }
+    }
+
+    void SetSelectedCell() {
+        SelectedCell = MovementManager.Instance.GetGridCell();
+    }
+
+}
