b1eca51211d4569a987403c0f852debca42965c0
TriDivide <liamsmitha@gmail.com>
2018-11-05 22:35:31 +0000

    - created the base models for the base building system

---
+++ RNDProject/Assets/BaseBuildingGame/DataModel/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+
+    public enum TileType {
+        Dirt,
+        Stone,
+        Paved,
+        RunningWater,
+        StillWater,
+        Sand,
+        ShallowWater,
+        Grass,
+        Foundations
+    }
+
+    TileType type = TileType.Dirt;
+
+    
+}
